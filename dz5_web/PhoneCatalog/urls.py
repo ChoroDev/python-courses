da2ff569df759d7399d69738bedaa1771b4bf5f1
lenovo <leksilonchikk@gmail.com>
2019-08-20 18:02:35 +0300

    dz5 done

--- dz5_web/PhoneCatalog/urls.py
+++ dz5_web/PhoneCatalog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'PhoneCatalog'
+app_name = 'phone catalog'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^([0-9]+)/$', views.phone, name='phone')
 ]
--- dz5_web/PhoneCatalog/views.py
+++ dz5_web/PhoneCatalog/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-
+from .models import PhoneCatalog
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    message = None
+    if "message" in request.GET:
+        message = request.GET["message"]
+    
+    return render(request, "index.html", {
+        "phones":
+            PhoneCatalog.objects.order_by('-reg_date')[:5],
+        "message": message
+    })
+
+
+def phone(request, phone_id):
+    error_message = None
+    if "error_message" in request.GET:
+        error_message = request.GET["error_message"]
+    return render(request, "phone.html", {
+        "phone": get_object_or_404(PhoneCatalog, pk=phone_id),
+        "error_message": error_message
+    })
