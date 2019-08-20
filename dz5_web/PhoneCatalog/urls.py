ba357d5c05788cc9c3d28693c1984da24bfb3562
lenovo <leksilonchikk@gmail.com>
2019-08-20 18:30:59 +0300

    dz6 done

--- dz5_web/PhoneCatalog/urls.py
+++ dz5_web/PhoneCatalog/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'phone catalog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^([0-9]+)/$', views.phone, name='phone')
+    url(r'^([0-9]+)/$', views.phone, name='phone'),
+    url(r'^register/$', views.RegisterFormView.as_view()),
+    url(r'^login/$', views.LoginFormView.as_view()),
+    url(r'^logout/$', views.LogoutView.as_view()),
+    url(r'^password-change/', views.PasswordChangeView.as_view()),
 ]
--- dz5_web/PhoneCatalog/views.py
+++ dz5_web/PhoneCatalog/views.py
@@ -1,6 +1,56 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import PhoneCatalog
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.views.generic.base import View
+from django.contrib.auth import logout
+from django.contrib.auth.forms import PasswordChangeForm
+
+app_url = "/phone_catalog/"
+
+class PasswordChangeView(FormView):
+    form_class = PasswordChangeForm
+    template_name = 'reg/password_change_form.html'
+    success_url = app_url + 'login/'
+    def get_form_kwargs(self):
+        kwargs = super(PasswordChangeView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        if self.request.method == 'POST':
+            kwargs['data'] = self.request.POST
+        return kwargs
+    def form_valid(self, form):
+        form.save()
+        return super(PasswordChangeView, self).form_valid(form)
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(app_url)
+
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+    template_name = "reg/login.html"
+    success_url = app_url
+    def form_valid(self, form):
+        self.user = form.get_user()
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+    success_url = app_url + "login/"
+    template_name = "reg/register.html"
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterFormView, self).form_valid(form)
+
 
 def index(request):
     message = None
